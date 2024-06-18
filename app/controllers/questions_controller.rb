6d68502e015ad456f6fe80517767b8b71d1d7b0d
Farouq Jalabi <farouq@example.com>
2024-06-18 09:35:18 +0200

    Result page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,30 @@
 class QuestionsController < ApplicationController
 
   def index
-    @questions = Question.all
+    #  @questions = Question.all
+    @questions = [
+        { id: 1, content: "How outgoing are you?" },
+        { id: 2, content: "How organized are you?" },
+        { id: 3, content: "How adventurous are you?" }
+      ]
+    @answers = Array.new(@questions.length)
   end
 
   def calculate_answer
-    puts "Farouq Jalabi"
-    redirect_to result_path(category_1: params["name"], category_2: "Test")
+    test_score = 50
+    @answers = params[:answers]
+
+    total_score = 0
+
+    @answers.each do |question_index, selected_options|
+      selected_options = [selected_options] unless selected_options.is_a?(Array)
+
+      question_score = selected_options.map(&:to_i).sum
+
+      total_score += question_score
+    end
+
+    # Result page expects category_{name} and a score
+    redirect_to result_path(category_total_score: total_score, category_2: 25)
   end
 end
