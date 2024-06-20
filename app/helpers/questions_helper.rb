94166eb7c42f3327a1445600ac57a1c183f9aa27
FarouqJalabi <farouqjalabi@gmail.com>
2024-06-20 08:54:19 +0200

    Description header

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,12 +1,17 @@
 module QuestionsHelper
   def percentage_to_label(percentage)
-    if percentage >= 0.0 and percentage <= 40.0
-      "Low"
-    elsif percentage >= 40.0 and percentage <= 60.0
-      "Average"
-    else
-    "High"
+    if percentage >= 0.0 and percentage <= 39.0
+      "very unlikely"
+    elsif percentage >= 40.0 and percentage <= 59.0
+      "unlikely"
+    elsif percentage >= 60.0 and percentage <= 79.0
+      "likely"
     end
-
+    "very likely"
   end
+
+
+
+
+
 end
