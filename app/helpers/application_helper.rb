bd596167b419a956e3ac88d47cbf4d428ec09dd9
FarouqJalabi <farouqjalabi@gmail.com>
2024-06-24 16:12:27 +0200

    Nav bar bug with domain name test-autism

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
   def on_home?
-    !(request.url.include? 'about') && !(request.url.include? 'test')
+    !(request.url.include? 'about') && !(request.url.include? '/test')
   end
   def on_test?
-    request.url.include? 'test'
+    request.url.include? '/test'
   end
   def on_about?
+    # Doesn't work with #about
     request.url.include? 'about'
   end
 end
