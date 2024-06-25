cf18046b9bade7f872de875518b6ba5926ddfbe0
FarouqJalabi <farouqjalabi@gmail.com>
2024-06-25 16:08:05 +0200

    Error page rendered inside layout

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,6 +1,6 @@
 class ErrorsController < ApplicationController
     def not_found
-      render file: Rails.public_path.join('404.html'), status: :not_found, layout: false
+      render file: Rails.public_path.join('404.html'), status: :not_found
     end
   end
   
