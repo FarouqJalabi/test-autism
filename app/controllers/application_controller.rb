d1e2623b3b78c079b24ee0271c5101dd251e64d9
FarouqJalabi <farouqjalabi@gmail.com>
2024-06-25 16:47:29 +0200

    No layout on 404 and home link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
     def not_found!
-        render file: "#{Rails.root}/public/404.html", status: :not_found
+        render file: "#{Rails.root}/public/404.html", layout: false, status: :not_found
       end
 end
