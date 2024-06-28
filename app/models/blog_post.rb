1ee3a111af17dc6cfe596dec7edaa8e9f0b8d51e
Farouq Jalabi <farouq@example.com>
2024-06-28 10:46:21 +0200

    Added image alt

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,6 +1,7 @@
 class BlogPost < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :content
-  validates_presence_of :image
+  validates_presence_of :image_src
+  validates_presence_of :image_alt
   validates_presence_of :time_to_read
 end
