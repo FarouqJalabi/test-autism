84bd2a4bac3675d4e7f4de17ed568d1d176eb573
fogadOg <fogadm06@gmail.com>
2024-06-28 11:12:44 +0200

    adding daisyUI and tailwind

---
+++ public/assets/controllers/questions_controller-d3ea94e85df441f8def97110e2e396595390a6d0d373b2b4f0a5721e50e5be81.js
@@ -0,0 +1,38 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    this.order = Number(this.element.dataset.order);
+    this.totalQuestions = Number(this.element.dataset.total);
+    this.currentQuestionContainer = this.element.closest('.question_container');
+  }
+
+  radio_clicked() {
+    if (this.order !== this.totalQuestions){
+      this.currentQuestionContainer.classList.add('slide-out-left');
+      this.currentQuestionContainer.classList.remove('current');
+    }
+    if (this.order === this.totalQuestions){
+      document.querySelector(".submit_button").style.visibility = "visible"
+    }
+
+    this.progress_bar(this.totalQuestions, this.order + 1);
+  }
+
+  back_clicked() {
+    let targetIndex = this.order-1 ;
+
+    let targetBoxContainer = document.querySelector('.question_container .content .box[data-order="' + targetIndex + '"]');
+    let targetQuestionContainer = targetBoxContainer.closest(".question_container");
+
+    targetQuestionContainer.classList.add('current');
+    this.currentQuestionContainer.classList.remove('slide-out-left');
+  }
+
+  progress_bar(number_of_question, current_question) {
+    let progress = (current_question / number_of_question) * 100;
+
+    let progressBarFill = document.querySelector('.progress-bar-fill');
+    progressBarFill.style.width = `${progress}%`;
+  }
+};
