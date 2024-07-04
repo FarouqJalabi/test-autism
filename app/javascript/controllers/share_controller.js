c887ce753a3a2ff0e709b777109c6c4be1623ed2
Farouq Jalabi <farouq@example.com>
2024-07-04 15:01:33 +0200

    data label

--- app/javascript/controllers/share_controller.js
+++ app/javascript/controllers/share_controller.js
@@ -3,13 +3,17 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     // un-hide if not on mobile
-    this.element.classList.remove(navigator.canShare() ? "hidden" : "")
+    if (navigator.canShare){
+      this.element.classList.remove("hidden")
+    }
 
   }
 
   async share_clicked() {
-    if (navigator.canShare()){
-        await navigator.share({title:"Test-Autism", text:"My results on Autism test", url: "https://test-autism.rubynor.com/en"});
+    if (navigator.canShare){
+        let share_data = {title: "Test-Autism", text: "My results on Autism test", url: "https://test-autism.rubynor.com/en"}
+        await navigator.share(share_data);
+        console.log(share_data)
     }
   }
 }
