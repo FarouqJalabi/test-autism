c223dc7787e119756ac07d8b8c23031a3ccd480e
Farouq Jalabi <farouq@example.com>
2024-07-04 10:54:06 +0200

    New labels

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -3,12 +3,12 @@ module QuestionsHelper
     question.test.questions.count
   end
   def percentage_to_label(percentage)
-    if percentage >= 0.0 and percentage <= 30.0
+    if percentage >= 0.0 and percentage <= 20.0
       t("result.label1")
-    elsif percentage >= 40.0 and percentage <= 50.0
+    elsif percentage >= 30.0 and percentage <= 40.0
       t("result.label2")
 
-    elsif percentage >= 60.0 and percentage <= 70.0
+    elsif percentage >= 50.0 and percentage <= 60.0
       t("result.label3")
     else
       t("result.label4")
@@ -18,11 +18,11 @@ module QuestionsHelper
   # For now we have descriptions hard coded
   # If we get several tests they will be coded into the test themselves
   def percentage_to_description(percentage)
-    if percentage >= 0.0 and percentage <= 30.0
+    if percentage >= 0.0 and percentage <= 20.0
       t("result.description1")
-    elsif percentage >= 40.0 and percentage <= 50.0
+    elsif percentage >= 30.0 and percentage <= 40.0
       t("result.description2")
-    elsif percentage >= 60.0 and percentage <= 70.0
+    elsif percentage >= 50.0 and percentage <= 60.0
       t("result.description3")
     else
       t("result.description4")
