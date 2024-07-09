52cbb405c9a46d92228cbe6f6e4ad8783b74ef15
Farouq Jalabi <farouq@example.com>
2024-07-09 11:39:19 +0200

    Replaced test-autism.rubynor.com -> test-autism.com

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "https://test-autism.rubynor.com"
+SitemapGenerator::Sitemap.default_host = "https://test-autism.com"
 SitemapGenerator::Sitemap.compress = false
 
 SitemapGenerator::Sitemap.create do
