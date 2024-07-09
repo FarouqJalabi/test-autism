8e0b1f56c482f8d468fb3ce8d9aedad2ca500d57
Farouq Jalabi <farouq@example.com>
2024-07-09 08:49:03 +0200

    ranges missed some spots

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -3,24 +3,24 @@ module QuestionsHelper
     question.test.questions.count
   end
   def percentage_to_header(percentage)
-    if percentage >= 0.0 and percentage <= 20.0
+    if percentage >= 0.0 and percentage <= 29.9
       t("result.fullLabel1")
-    elsif percentage >= 30.0 and percentage <= 40.0
+    elsif percentage >= 30.0 and percentage <= 49.9
       t("result.fullLabel2")
 
-    elsif percentage >= 50.0 and percentage <= 60.0
+    elsif percentage >= 50.0 and percentage <= 69.9
       t("result.fullLabel3")
     else
       t("result.fullLabel4")
     end
   end
   def percentage_to_label(percentage)
-    if percentage >= 0.0 and percentage <= 20.0
+    if percentage >= 0.0 and percentage <= 29.0
       t("result.label1")
-    elsif percentage >= 30.0 and percentage <= 40.0
+    elsif percentage >= 30.0 and percentage <= 49.0
       t("result.label2")
 
-    elsif percentage >= 50.0 and percentage <= 60.0
+    elsif percentage >= 50.0 and percentage <= 69.9
       t("result.label3")
     else
       t("result.label4")
@@ -30,11 +30,11 @@ module QuestionsHelper
   # For now we have descriptions hard coded
   # If we get several tests they will be coded into the test themselves
   def percentage_to_description(percentage)
-    if percentage >= 0.0 and percentage <= 20.0
+    if percentage >= 0.0 and percentage <= 30.0
       t("result.description1")
-    elsif percentage >= 30.0 and percentage <= 40.0
+    elsif percentage >= 30.0 and percentage <= 49.0
       t("result.description2")
-    elsif percentage >= 50.0 and percentage <= 60.0
+    elsif percentage >= 50.0 and percentage <= 69.9
       t("result.description3")
     else
       t("result.description4")
@@ -42,11 +42,11 @@ module QuestionsHelper
   end
 
   def percentage_to_share(percentage)
-    if percentage >= 0.0 and percentage <= 20.0
+    if percentage >= 0.0 and percentage <= 30.0
       t("result.share1")
-    elsif percentage >= 30.0 and percentage <= 40.0
+    elsif percentage >= 30.0 and percentage <= 49.0
       t("result.share2")
-    elsif percentage >= 50.0 and percentage <= 60.0
+    elsif percentage >= 50.0 and percentage <= 69.9
       t("result.share3")
     else
       t("result.share4")
