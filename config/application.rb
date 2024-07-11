6bb0643ddf09b4d340716fcefd3c01b6633edcb2
fogadOg <fogadm06@gmail.com>
2024-07-11 09:26:11 +0200

    setting initialize_on_precompile to false

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Questioner
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
