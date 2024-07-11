2988b32f8dd3c0c9fb514be3ebbc7423e0efa995
fogadOg <fogadm06@gmail.com>
2024-07-11 09:29:02 +0200

    removing line

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Questioner
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
