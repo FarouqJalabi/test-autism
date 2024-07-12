e8edff0c6c2fb0f9b4b0c7f399e695397a3a1a00
FarouqJalabi <farouqjalabi@gmail.com>
2024-07-12 10:05:36 +0200

    Able to share to twitter without navigator

--- app/javascript/controllers/share_controller.js
+++ app/javascript/controllers/share_controller.js
@@ -1,13 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = [ "navigatorButtons", "shareButtons" ]
+
   connect() {
+    console.log(this.shareButtonsTarget)
     // un-hide if not on mobile
     if (navigator.canShare){
-      this.element.classList.remove("hidden")
-
+      this.navigatorButtonsTarget.classList.remove("hidden")
+    } else {
+      this.shareButtonsTarget.classList.remove("hidden")
     }
-
   }
 
   async share_clicked() {
