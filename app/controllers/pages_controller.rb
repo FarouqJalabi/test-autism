58d1cd1fc81bf586b1e58facd9c3a2d36c63a014
fogadOg <fogadm06@gmail.com>
2024-07-15 12:57:17 +0200

    changing variable name

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,15 +2,15 @@ class PagesController < ApplicationController
   def index
     @blogs = Blog.all.sort_by(&:updated_at).reverse
     @tests_taken = TestScore.count   
-    @above_50_count = TestScore.where('score > ?', 60).count
-    @under_50_count = TestScore.where('score < ?', 30).count
+    @very_likely = TestScore.where('score > ?', 60).count
+    @likely = TestScore.where('score < ?', 30).count
     
     if @tests_taken > 0
-      @above_50_percentage = (@above_50_count.to_f / @tests_taken * 100).round(2)
-      @under_50_percentage = (@under_50_count.to_f / @tests_taken * 100).round(2)
+      @very_likely_percentage = (@very_likely.to_f / @tests_taken * 100).round(2)
+      @likely_percentage = (@likely.to_f / @tests_taken * 100).round(2)
     else
-      @above_50_percentage = 0.0
-      @under_50_percentage = 0.0
+      @very_likely_percentage = 0.0
+      @likely_percentage = 0.0
     end
   end
 
