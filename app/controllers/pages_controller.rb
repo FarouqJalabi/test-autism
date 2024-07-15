e9b6b5e18fabb8fb94e64c6780a56c6417041f96
fogadOg <fogadm06@gmail.com>
2024-07-15 12:54:38 +0200

    calculating stats

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,19 @@
 class PagesController < ApplicationController
   def index
     @blogs = Blog.all.sort_by(&:updated_at).reverse
-
+    @tests_taken = TestScore.count   
+    @above_50_count = TestScore.where('score > ?', 60).count
+    @under_50_count = TestScore.where('score < ?', 30).count
+    
+    if @tests_taken > 0
+      @above_50_percentage = (@above_50_count.to_f / @tests_taken * 100).round(2)
+      @under_50_percentage = (@under_50_count.to_f / @tests_taken * 100).round(2)
+    else
+      @above_50_percentage = 0.0
+      @under_50_percentage = 0.0
+    end
   end
+
 
   
   def translations
