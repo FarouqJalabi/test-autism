dda5936189659d720861afb96cab257f3324177a
fogadOg <fogadm06@gmail.com>
2024-07-15 15:25:35 +0200

    addnign more stats

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,24 @@
 class PagesController < ApplicationController
   def index
     @blogs = Blog.all.sort_by(&:updated_at).reverse
-    @tests_taken = TestScore.count   
-    @very_likely = TestScore.where('score > ?', 60).count
-    @likely = TestScore.where('score < ?', 30).count
     
+    @tests_taken = TestScore.count   
+    
+    @very_unlikely_count = TestScore.where('score <= ?', 30).count
+    @unlikely_count = TestScore.where('score > ? AND score <= ?', 30, 50).count
+    @likely_count = TestScore.where('score > ? AND score <= ?', 50, 70).count
+    @very_likely_count = TestScore.where('score > ?', 70).count
+
     if @tests_taken > 0
-      @very_likely_percentage = (@very_likely.to_f / @tests_taken * 100).round(2)
-      @likely_percentage = (@likely.to_f / @tests_taken * 100).round(2)
+      @very_unlikely_percentage = (@very_unlikely_count.to_f / @tests_taken * 100).round(2)
+      @unlikely_percentage = (@unlikely_count.to_f / @tests_taken * 100).round(2)
+      @likely_percentage = (@likely_count.to_f / @tests_taken * 100).round(2)
+      @very_likely_percentage = (@very_likely_count.to_f / @tests_taken * 100).round(2)
     else
-      @very_likely_percentage = 0.0
+      @very_unlikely_percentage = 0.0
+      @unlikely_percentage = 0.0
       @likely_percentage = 0.0
+      @very_likely_percentage = 0.0
     end
   end
 
