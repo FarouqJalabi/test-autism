1a22e9a7d594b9984973b261f4f03ee09b53d2b5
fogadOg <fogadm06@gmail.com>
2024-07-15 11:28:31 +0200

    removing unused code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,30 +1,9 @@
 class PagesController < ApplicationController
   def index
     @blogs = Blog.all.sort_by(&:updated_at).reverse
-    @scoreRange = scoreRange()
 
   end
 
-  def scoreRange
-    scores = TestScore.all.pluck(:score)
-
-    total_scores = scores.size
-
-    increments = (1..10).map { |i| i * 10 }
-
-    @range_data = {}
-
-    scores.each do |score|
-      range = (score / 10.0).ceil * 10
-      range = 100 if range > 100
-      @range_data[range] ||= { count: 0, percentage: 0 }
-      @range_data[range][:count] += 1
-    end
-
-    @range_data.each do |range, data|
-      data[:percentage] = (data[:count].to_f / total_scores * 100).round(2)
-    end
-  end
   
   def translations
   end
