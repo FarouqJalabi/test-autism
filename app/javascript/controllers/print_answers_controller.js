c5b59033d70dcab04bf73dabcda687ce31271bb8
Farouq Jalabi <farouq@example.com>
2024-07-16 11:20:37 +0200

    Clearing localstorage when visited result and started new test

--- app/javascript/controllers/print_answers_controller.js
+++ app/javascript/controllers/print_answers_controller.js
@@ -2,11 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 // Getting answers from localstorage and clearing localstorage
 export default class extends Controller {
   connect() {
+    localStorage.setItem("viewed_result", "true")
+
     Object.keys(localStorage).forEach(
     function(key){
+      if (key.includes("question_")){
       let radio_button = document.getElementById(localStorage.getItem(key));
       radio_button.checked = true
-      console.log(radio_button.checked)
+      }
     });
   }
 
