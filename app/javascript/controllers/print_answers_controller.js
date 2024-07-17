f3e3b3da785cb21ee52d7935967a2bf50a37bd0a
FarouqJalabi <farouqjalabi@gmail.com>
2024-07-17 14:02:10 +0200

    Creating score successfully

--- app/javascript/controllers/print_answers_controller.js
+++ app/javascript/controllers/print_answers_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 // Getting answers from localstorage and clearing localstorage
 export default class extends Controller {
   connect() {
-    localStorage.setItem("viewed_result", "true")
+    // localStorage.setItem("viewed_result", "true")
 
     Object.keys(localStorage).forEach(
     function(key){
