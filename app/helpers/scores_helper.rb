052491b3b719ee351e9ec4f37a44e04c29b6e006
FarouqJalabi <farouqjalabi@gmail.com>
2024-07-17 10:39:58 +0200

    small refactor

--- app/helpers/scores_helper.rb
+++ app/helpers/scores_helper.rb
@@ -4,19 +4,19 @@ module ScoresHelper
   end
 
   def percentage_to_header(percentage)
-    t("result.fullLabel"+(get_test_bracket(percentage)+1).to_s)
+    t("result.fullLabel"+get_test_bracket(percentage).to_s)
   end
 
   def percentage_to_label(percentage)
-    t("result.label"+(get_test_bracket(percentage)+1).to_s)
+    t("result.label"+get_test_bracket(percentage).to_s)
   end
 
   def percentage_to_description(percentage)
-    t("result.description"+(get_test_bracket(percentage)+1).to_s)
+    t("result.description"+get_test_bracket(percentage).to_s)
   end
 
   def percentage_to_share(percentage)
-    t("result.share"+(get_test_bracket(percentage)+1).to_s)
+    t("result.share"+get_test_bracket(percentage).to_s)
   end
 
 end
