32d75c218b318aac193321821b69fe38ef535d9b
FarouqJalabi <farouqjalabi@gmail.com>
2024-07-17 08:52:19 +0200

    Blog.recent retreving 5 newest blogs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @blogs = Blog.all.sort_by(&:updated_at).reverse
+    @blogs = Blog.recent
     
     @tests_taken = TestScore.count
     score_counts = count_scores
