430e1e3eb50883054cc5f72043bc4b28470f9a2d
FarouqJalabi <farouqjalabi@gmail.com>
2024-07-17 10:14:46 +0200

    Global test brackets function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,13 @@ class ApplicationController < ActionController::Base
   
   def not_found!
     render file: "#{Rails.root}/public/404.html", layout: false, status: :not_found
-end
+  end
+
+  def get_test_bracket(x)
+    # very unlikely, unlikely, likely, very unlikely
+    brackets = [0.0..29.9, 30.0..49.9, 50.0..69.9, 70.0..100.0].freeze
+
+    index = brackets.index { |range| range.include? x }
+    index.nil? ? index : -1
+  end
 end
