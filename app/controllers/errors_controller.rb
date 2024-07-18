6c9cf7d7bded52fd631d2a51f2c4df195381fd4d
FarouqJalabi <farouqjalabi@gmail.com>
2024-07-18 09:45:56 +0200

    Removed layout from errors

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,13 +1,13 @@
 class ErrorsController < ApplicationController
   def not_found
-    render status: :not_found
+    render status: :not_found, layout: false
   end
 
   def internal_server_error
-    render status: :internal_server_error
+    render status: :internal_server_error, layout: false
   end
 
   def unprocessable_content
-    render status: :unprocessable_content
+    render status: :unprocessable_content, layout: false
   end
 end
